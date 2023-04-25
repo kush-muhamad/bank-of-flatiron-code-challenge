5dfcc7b8e4e49d6cf263d68d141ef2ad6d19efd9
kush Muhamad <kushmohammed98@yahoo.com>
2023-04-25 20:56:22 +0300

    first-commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./index.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+           
+      <h2>The Royal Bank of Flatiron</h2>
+        
+      </header>
+
+      {/* <TransactionsContainer /> */}
+    </div>
+  );
+}
+
+export default App;
